2e29737ba3a7572a13323ca649dce5fb846c8d8d
PC-19 <PC-19@DESKTOP-QGORT2A>
2023-01-03 18:17:35 +0900

    p62

---
+++ src/chapter02/VariableInitializationExample.java
@@ -0,0 +1,16 @@
+package chapter02;
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		// 변수 value 선언 및 초기화
+		int value = 10;
+		
+		// 연산의 결과값을 변수 result의 초기값으로 저장
+		int result = value + 30;
+		
+		// 변수 result 값을 읽고 콘솔에 출력
+		System.out.println(result);
+	}
+}
+
+
